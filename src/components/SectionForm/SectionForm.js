24db590bb567907d2082ee4b549cb122696ec534
mathieu <vanderlyndenmathieu@gmail.com>
2023-11-13 11:21:42 +0100

    ajout input onchange add input

--- src/components/SectionForm/SectionForm.js
+++ src/components/SectionForm/SectionForm.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const SectionForm = () => {
-  return (
-    <div>SectionForm</div>
-  )
-}
+  const [kpiName, setKpiName] = useState("");
 
-export default SectionForm
+  return (
+    <div className="mainContainer">
+      <div className="cardSettings flex flexCol gp32">
+        <div>
+          <h4>Card settings</h4>
+        </div>
+
+        <div className="allElementsInput flex flexCol gp24">
+          <div className=" flex flexCol gp8">
+            <div className="fixContent">
+              <p className="BoldText">KPI name</p>
+            </div>
+
+            <div className="w100">
+              <input
+                className="input w100"
+                placeholder="Element name"
+                type="text"
+                value={kpiName}
+                onChange={(e) => setKpiName(e.target.value)}
+              />
+            </div>
+          </div>
+
+          {kpiName && (
+            <div className="flex flexCol gp8 tooltipBox">
+              <div className="fixContent">
+                <p className="BoldText">Tooltip</p>
+              </div>
+
+              <div className="w100">
+                <input
+                  className="input w100"
+                  placeholder="Element name"
+                  type="text"
+                />
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SectionForm;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/Header/Header";
 import SectionAddKPI from "../components/SectionAddKPI/SectionAddKPI";
+import SectionForm from "../components/SectionForm/SectionForm";
 
 const Home = () => {
   return (
     <>
       <Header />
       <SectionAddKPI />
+      <SectionForm />
     </>
   );
 };
